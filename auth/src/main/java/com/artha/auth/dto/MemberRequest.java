419d3e91064d4448d2556f91bfadc6401add2176
JasmitaVekariya <pjasmita7@gmail.com>
2026-02-13 14:06:38 +0530

    Local changes before pull

--- auth/src/main/java/com/artha/auth/dto/MemberRequest.java
+++ auth/src/main/java/com/artha/auth/dto/MemberRequest.java
@@ -1,10 +1,10 @@
 package com.artha.auth.dto;
 
-import com.artha.auth.entity.CompanyRole;
+import com.artha.auth.entity.UserCompanyRole;
 import lombok.Data;
 
 @Data
 public class MemberRequest {
     private String userId;
-    private CompanyRole role;
+    private UserCompanyRole role;
 }

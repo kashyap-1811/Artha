aa1a52df1115e92147aeea3c8517166cbf9c84b4
kashyap-1811 <kashyaprupareliya1811@gmail.com>
2026-02-21 23:20:19 +0530

    Change in response

--- user-service/src/main/java/com/artha/user/services/IUserService.java
+++ user-service/src/main/java/com/artha/user/services/IUserService.java
@@ -1,10 +1,13 @@
 package com.artha.user.services;
 
+import com.artha.user.entity.Company;
 import com.artha.user.entity.User;
 
 
 public interface IUserService {
     User create(User u);
+
+    Company ensurePersonalCompany(String userId);
 
     User update(User u);
 

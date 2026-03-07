80d1b76f92f2ff58694cf79664e58ebf49c619b5
JasmitaVekariya <pjasmita7@gmail.com>
2026-03-08 00:50:59 +0530

    fix: remove hardcoded MongoDB credentials from config.py, use .env instead

--- analysis-service/app/core/config.py
+++ analysis-service/app/core/config.py
@@ -1,5 +1,8 @@
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 API_GATEWAY_URL = os.getenv("API_GATEWAY_URL", "http://localhost:8080")
-MONGO_DETAILS = os.getenv("MONGO_DETAILS", "mongodb+srv://pjasmita7_db_user:UGC2rixqoIncb5hQ@cluster0.lwzn7a9.mongodb.net/?appName=Cluster0")
+MONGO_DETAILS = os.getenv("MONGO_DETAILS")
 EUREKA_SERVER = os.getenv("EUREKA_SERVER", "http://localhost:8761/eureka/")

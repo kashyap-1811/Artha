7e2a3b2d69cf44d3cb009040039f2b797424a6ef
kashyap-1811 <kashyaprupareliya1811@gmail.com>
2026-06-09 18:29:25 +0530

    fix: re-run pipeline after testing

--- budget/src/main/java/com/artha/budget/BudgetApplication.java
+++ budget/src/main/java/com/artha/budget/BudgetApplication.java
@@ -3,9 +3,6 @@ package com.artha.budget;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.stereotype.Component;
-
-import jakarta.annotation.PostConstruct;
 
 @SpringBootApplication
 @EnableCaching
@@ -13,13 +10,5 @@ public class BudgetApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BudgetApplication.class, args);
-	}
-
-	@Component
-	public static class StartupFailureTrigger {
-		@PostConstruct
-		public void init() {
-			throw new RuntimeException("Simulated startup failure for budget-service rollback testing!");
-		}
 	}
 }
